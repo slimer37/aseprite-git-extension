02aa50afcaf47cb996ffa0d655ec16a7e6452d74
Alfred <59073967+slimer37@users.noreply.github.com>
2022-10-25 19:56:23 -0700

    Show available commits in dialog

--- src/viewer.lua
+++ src/viewer.lua
@@ -8,34 +8,49 @@ function viewer.open(spriteFile)
         return
     end
 
+    local commits = git.getCommits(repo, relFile)
+    local options = { "1. HEAD" }
+
+    for i, v in ipairs(commits) do
+        table.insert(options, tostring(i + 1) .. ". " .. string.sub(v.sha, 1, 5) .. string.gsub(" | $author: $message ($date)", "%$(%w+)", v))
+    end
+
     local dlg = Dialog{ title="View commit" }
-        :label{ text="Enter the branch or first part of the commit SHA:" }
-        :entry{ id="identifier" }
+        :label{ text="Select the commit:" }
+        :combobox{
+            id="identifier",
+            option="HEAD",
+            options=options
+        }
         :button{ id="go", text="Go" }
 
-    local bounds = dlg.bounds
-    dlg:show{
-        bounds=Rectangle(100, 100, bounds.width + 50, bounds.height + 50)
-    }
+    dlg:show()
 
     local data = dlg.data
 
     if data.go then
-        local frameNumber = app.activeFrame.frameNumber;
-
-        local success, file = git.loadFromCommit(data.identifier, repo, relFile)
+        local idIndex = tonumber(string.sub(data.identifier, 1, 1)) - 1
+        local identifier = "HEAD"
+        
+        if idIndex > 0 then
+            identifier = commits[idIndex].sha
+        end
+        
+        local success, file = git.loadFromCommit(identifier, repo, relFile)
         
         if not success then
             app.alert{
                 title="Invalid Identifier",
-                text="'" .. data.identifier .. ":" .. relFile .. "' was not valid."
+                text="'" .. string.sub(identifier, 1, 5) .. ":" .. relFile .. "' was not valid."
             }
             return
         end
 
+        local frameNumber = app.activeFrame.frameNumber;
+        
         local sprite = app.open(file)
         os.delete(file)
-        sprite.filename = data.identifier .. ">" .. app.fs.fileName(relFile)
+        sprite.filename = string.sub(identifier, 1, 5) .. " > " .. app.fs.fileName(relFile)
 
         if (#app.activeSprite.frames >= frameNumber) then
             app.activeFrame = app.activeSprite.frames[frameNumber];
