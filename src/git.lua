7b2b58d5e2b813548a13f5872a055ca7ef7c9640
Alfred <59073967+slimer37@users.noreply.github.com>
2022-10-25 19:55:58 -0700

    Create a method to get git commits of a file

--- src/git.lua
+++ src/git.lua
@@ -19,3 +19,40 @@ function git.getRepo(file)
 
     return repo, relativeFile
 end
+
+-- http://lua-users.org/wiki/StringRecipes
+function string.starts(string, start)
+    return string.sub(string, 1, #start) == start
+end
+
+-- Runs 'git log --follow' on the file and returns a table of objects with sha, author, message, and date properties.
+function git.getCommits(repo, location)
+    local cmd = "cd " .. repo .. " & git log --follow " .. location
+    local output = os.capture(cmd, true)
+
+    local lines = {}
+    for s in output:gmatch("[^\r\n]+") do
+        table.insert(lines, s)
+    end
+
+    local commits = {}
+    local sha = nil
+
+    for i, line in ipairs(lines) do
+        if string.starts(line, "commit") then
+            sha = string.sub(line, #"commit " + 1)
+            table.insert(commits, {})
+            commits[#commits].sha = sha
+        elseif string.starts(line, "Author:") then
+            -- Trim email portion with regex
+            commits[#commits].author = string.sub(line, #"Author: " + 1, string.find(line, " <.*@.*>") - 1)
+        elseif string.starts(line, "Date:") then
+            commits[#commits].date = string.sub(line, #"Date:   " + 1, string.find(line, "-") - 2)
+        elseif line ~= "" and commits[#commits].message == nil then
+            -- Trim space at beginning of commit message
+            commits[#commits].message = string.sub(line, 5)
+        end
+    end
+
+    return commits
+end
